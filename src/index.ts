385d17c21aa39e93597948294040e8f3ab8d40a7
Jim Allanson <jim@amplience.com>
2019-11-13 12:53:48 +0000

    First steps loading GrapesJS in DC

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
 import { init } from 'dc-extensions-sdk';
 import { $ } from './bling';
+import * as grapesjs from 'grapesjs'
 
 (async () => {
   try {
     var sdk = await init<string>();
+
+    var editor = grapesjs.init({
+      height: '100%',
+      showOffsets: 1,
+      noticeOnUnload: 0,
+      storageManager: { autoload: 0 },
+      container: '#gjs',
+      fromElement: true,
+      plugins: [],
+    });
+
     var value = await sdk.field.getValue();
     const field: HTMLInputElement = $('#field');
     if(value !== undefined) {
@@ -16,6 +28,7 @@ import { $ } from './bling';
     };
     field.on('keyup', _ => setContent(field.value));
   } catch (e) {
+    console.log(e);
     const error:HTMLHeadingElement = $('#error');
     error.classList.add('show');
   }
